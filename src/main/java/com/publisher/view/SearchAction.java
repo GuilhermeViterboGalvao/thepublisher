2a12c677e2e1e629a595943428087ecd07b842bb
Guilherme <Guilherme@192.168.0.104>
2015-11-11 13:55:32 -0200

    Ajustes para listar apenas matérias publicadas.

--- src/main/java/com/publisher/view/SearchAction.java
+++ src/main/java/com/publisher/view/SearchAction.java
@@ -28,7 +28,7 @@ public class SearchAction extends ActionSupport implements ViewAction {
 	@Override
 	public String execute() throws Exception {
 		if (query != null && !query.isEmpty()) {
-			articles = articleService.search(query, currentPage, pageSize).getResult();
+			articles = articleService.search(query, currentPage, pageSize, true).getResult();
 		}
 		return SUCCESS;
 	}
