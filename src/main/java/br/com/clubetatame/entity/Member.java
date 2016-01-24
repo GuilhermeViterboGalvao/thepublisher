9cb749d48cbfbeb3afefaed2e7e4755b31cb33fc
Guilherme <Guilherme@mbpdeguilherme3>
2016-01-24 13:56:20 -0200

    Criado a entidade Membro e seus respectivos Service e ServiceImpementation.

---
+++ src/main/java/br/com/clubetatame/entity/Member.java
@@ -0,0 +1,240 @@
+package br.com.clubetatame.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.search.annotations.DateBridge;
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.IndexedEmbedded;
+import org.hibernate.search.annotations.Resolution;
+import org.hibernate.search.annotations.SortableField;
+import org.hibernate.search.annotations.Store;
+import com.publisher.entity.Account;
+
+@Entity
+@Indexed
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Member implements Serializable {
+
+	private static final long serialVersionUID = 5984515100458191997L;
+
+	@Id
+	@DocumentId
+    @GeneratedValue(strategy = GenerationType.AUTO)	
+	private Long id;
+	
+	@Field
+	private String name;
+	
+	@Field
+	private String document;
+	
+	@Field
+	private String email;
+	
+	private boolean gender;
+	
+	private Date birth;
+	
+	@Field
+	private String address;
+	
+	@Field
+	private String cep;
+	
+	private String hash;
+	
+	private Long fbid;
+	
+	private String facebookAccessToken;
+	
+	private Date facebookAccessTokenExpiration;
+	
+	@Field(index = Index.YES, store = Store.YES)
+	private boolean active = true;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @IndexedEmbedded(includeEmbeddedObjectId=true)
+	private Account createdBy;
+	
+    @SortableField
+    @DateBridge(resolution = Resolution.DAY)
+    @Field(index = Index.YES, store = Store.YES)    
+	private Date created;
+	
+    @ManyToOne(fetch = FetchType.LAZY)
+    @IndexedEmbedded(includeEmbeddedObjectId=true)	
+	private Account lastModifiedBy;
+	
+	private Date lastModified;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCEP() {
+		return cep;
+	}
+
+	public void setCEP(String cep) {
+		this.cep = cep;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	public Long getFbid() {
+		return fbid;
+	}
+
+	public void setFbid(Long fbid) {
+		this.fbid = fbid;
+	}
+
+	public String getFacebookAccessToken() {
+		return facebookAccessToken;
+	}
+
+	public void setFacebookAccessToken(String facebookAccessToken) {
+		this.facebookAccessToken = facebookAccessToken;
+	}
+
+	public Date getFacebookAccessTokenExpiration() {
+		return facebookAccessTokenExpiration;
+	}
+
+	public void setFacebookAccessTokenExpiration(Date facebookAccessTokenExpiration) {
+		this.facebookAccessTokenExpiration = facebookAccessTokenExpiration;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Account getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Account createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Account getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(Account lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	public Date getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(Date lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	@Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+	
+	@Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Member)) {
+            return false;
+        }
+        Member other = (Member) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "br.com.clubetatame.entity.Member#" + String.valueOf(id);
+    }	
+}
