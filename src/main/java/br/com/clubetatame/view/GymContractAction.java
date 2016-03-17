5903f7210d7f79811a163f71b39409b92d6f2fbe
Wlad <Wlad@192.168.0.166>
2016-03-17 15:40:08 -0300

    Clube - ajustes

--- src/main/java/br/com/clubetatame/view/GymContractAction.java
+++ src/main/java/br/com/clubetatame/view/GymContractAction.java
@@ -4,22 +4,38 @@ import java.util.Collection;
 
 import br.com.clubetatame.entity.Gym;
 import br.com.clubetatame.entity.GymContract;
+import br.com.clubetatame.service.ContractService;
 import br.com.clubetatame.service.GymContractService;
 import br.com.clubetatame.service.GymService;
+import br.com.clubetatame.service.ProductService;
 
 public class GymContractAction extends AbstractAction<GymContract> {
 
 	private static final long serialVersionUID = -5054133042718478840L;
 	
-	private GymContractService service;
+	private ProductService productService;
 	
-	public void setGymContractService(GymContractService service) {
-		this.service = service;
+	public void setProductService(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	private GymService gymService;
+	
+	public void setGymService(GymService gymService) {
+		this.gymService = gymService;
+	}
+	
+	private ContractService<GymContract> contractService;
+	
+	public void setContractService(ContractService<GymContract> contractService) {
+		this.contractService = contractService;
+		this.contractService.setGenericClass(GymContract.class);
+		this.contractService.setEntityName(GymContract.class.getName());
 	}
 	
 	@Override
 	public GymContract getEntity(long id) {
-		return service.get(id);
+		return contractService.get(id);
 	}
 
 	@Override
@@ -29,8 +45,8 @@ public class GymContractAction extends AbstractAction<GymContract> {
 
 	@Override
 	public String getContentPath() {
-		String content = "/skins/clube/gym/list.jsp";
-		if(getId() > 0) content = "/skins/clube/gym/info.jsp"; 
+		String content = "/skins/clube/gym/contracts.jsp";
+		if(getId() > 0) content = "/skins/clube/gym/contract.jsp"; 
 		return content;
 	}
 	
